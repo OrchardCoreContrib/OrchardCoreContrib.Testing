cf34613a83d2e60f3125a116fd36ea5c6eeaf36d
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-04-29 15:00:10 +0300

    Move playwright.ps1 to OCC.Testing.UI

--- src/OrchardCoreContrib.Testing.UI/Infrastructure/WebApplicationFactoryFixture.cs
+++ src/OrchardCoreContrib.Testing.UI/Infrastructure/WebApplicationFactoryFixture.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Hosting.Server;
+﻿using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
