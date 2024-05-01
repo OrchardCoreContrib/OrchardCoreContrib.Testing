d770bd18e986dde1c14636fe39cdcfc768c3b242
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-05-02 00:07:11 +0300

    Refactor UITest abstraction & implementation APIs

---
+++ src/OrchardCoreContrib.Testing.UI/IUITest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace OrchardCoreContrib.Testing.UI;
+
+/// <summary>
+/// Represents a contract for UI test.
+/// </summary>
+public interface  IUITest : IAsyncLifetime
+{
+    /// <summary>
+    /// Gets or sets the browser instance to be used during the test.
+    /// </summary>
+    public IBrowser Browser { get; set; }
+
+    /// <summary>
+    /// Gets the options used during the test.
+    /// </summary>
+    public UITestOptions Options { get; }
+}
---
+++ src/OrchardCoreContrib.Testing.UI/UITestBaseOfT.cs
@@ -0,0 +1,17 @@
+﻿using OrchardCoreContrib.Testing.UI.Infrastructure;
+
+namespace OrchardCoreContrib.Testing.UI;
+
+/// <summary>
+/// Represents a base class for UI testing.
+/// </summary>
+/// <typeparam name="TStartup">The startup class that will be used as entry point.</typeparam>
+/// <param name="fixture">The <see cref="WebApplicationFactoryFixture{TStartup}"/>.</param>
+public abstract class UITestBase<TStartup>(WebApplicationFactoryFixture<TStartup> fixture, UITestOptions testOptions)
+    : UITestBase(testOptions) where TStartup : class
+{
+    /// <summary>
+    /// Gets the base URL used for the tested website.
+    /// </summary>
+    public string BaseUrl => fixture.ServerAddress;
+}
