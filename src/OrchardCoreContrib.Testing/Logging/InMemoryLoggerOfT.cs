c86e1923d7dc3edaf13e6bf15464dab8b7e04b7c
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2026-07-14 00:56:54 +0300

    InMemoryLogger

---
+++ src/OrchardCoreContrib.Testing/Logging/InMemoryLoggerOfT.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace OrchardCoreContrib.Testing.Logging;
+
+/// <summary>
+/// An in-memory logger implementation for testing purposes.
+/// </summary>
+/// <typeparam name="T">The type whose name is used for the logger category.</typeparam>
+public class InMemoryLogger<T> : InMemoryLogger, ILogger<T>;
---
+++ src/OrchardCoreContrib.Testing/Logging/LogEntry.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace OrchardCoreContrib.Testing.Logging;
+
+/// <summary>
+/// Represents a log record.
+/// </summary>
+/// <param name="Level">The log level.</param>
+/// <param name="EventId">The event ID.</param>
+/// <param name="Message">The log message.</param>
+/// <param name="Exception">The exception associated with the log entry, if any.</param>
+public record LogEntry(LogLevel Level, EventId EventId, string Message, Exception Exception);
